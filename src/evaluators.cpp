f3758c5e6b2ea72af0906f07bd4d7072d92f9d4e
Thomas Nagler <thomas.nagler@tum.de>
2016-03-30 16:41:03 +0200

    fix cdf of kde1d with bounded data

--- src/evaluators.cpp
+++ src/evaluators.cpp
@@ -3,30 +3,44 @@ using namespace Rcpp;
 
 
 // [[Rcpp::export]]
-NumericVector kern_gauss(const NumericVector x){
+NumericVector kern_gauss(const NumericVector x) {
     NumericVector out(x.size());
     for(int i = 0; i < x.size(); ++i){
         if((abs(x[i]) >= 5)){
             out[i] = 0;
-        } else{
-            out[i] = exp(- 0.5 * pow(x[i], 2)) / (sqrt(2 * M_PI)) / 0.9999994267; 
-        } 
+        } else {
+            // normalize by 0.9999994267 because of truncation
+            out[i] = exp(- 0.5 * pow(x[i], 2)) / (sqrt(2 * M_PI)) / 0.9999994267;
+        }
     }
     return out;
 }
 
 // [[Rcpp::export]]
 NumericVector ikern_gauss(const NumericVector x) {
-    return pnorm(x);
+    NumericVector out(x.size());
+    NumericVector xx(1);
+    for (int i = 0; i < x.size(); ++i) {
+        if (x[i] <= -5.0) {
+            out[i] = 0.0;
+        } else if (x[i] > 5.0) {
+            out[i] = 1.0;
+        } else {
+            xx[0] = x[i];
+            // pnorm(-5.0) = 2.866516e-07
+            out[i] = (pnorm(xx)[0] - 2.866516e-07) / 0.9999994267;
+        }
+    }
+    return out;
 }
 
 
 // [[Rcpp::export]]
-NumericVector eval_kde1d(const NumericVector xsort, 
-                         const NumericVector xev, 
-                         const double xmin, 
-                         const double xmax, 
-                         const double bw) 
+NumericVector eval_kde1d(const NumericVector xsort,
+                         const NumericVector xev,
+                         const double xmin,
+                         const double xmax,
+                         const double bw)
 {
     // The vector xsort has to be in ascending order !!!
     NumericVector tmp(xev.size());
@@ -34,7 +48,7 @@ NumericVector eval_kde1d(const NumericVector xsort,
     NumericVector xeveff(xev.size());
     NumericVector xevdst(xev.size());
     double n = xsort.size();
-    
+
     for (int i = 0; i < xev.size(); ++i) {
         if ((xev[i] < xmin) | (xev[i] > xmax)) {
             out[i] = 0;
@@ -48,63 +62,72 @@ NumericVector eval_kde1d(const NumericVector xsort,
             } else {
                 xeveff[i] = xev[i];
                 xevdst[i] = 0;
-            }  
+            }
             if (pow(xevdst[i], 2) > 5 * bw) {
                 out[i] = 0;
             } else {
-                tmp = kern_gauss((xsort - xeveff[i]) / bw); 
+                tmp = kern_gauss((xsort - xeveff[i]) / bw);
                 if(!(xmin != xmin))
                     tmp = tmp + kern_gauss((2 * xmin - xsort - xeveff[i]) / bw);
-                if(!(xmax != xmax)) 
-                    tmp = tmp + kern_gauss((2 * xmax - xsort - xeveff[i]) / bw); 
+                if(!(xmax != xmax))
+                    tmp = tmp + kern_gauss((2 * xmax - xsort - xeveff[i]) / bw);
                 out[i] = sum(tmp) / (n * bw) * exp(- 0.5 *  pow(xevdst[i], 2) / bw) ;
             }
-        }    
+        }
     }
     return out;
 }
 
 // [[Rcpp::export]]
-NumericVector eval_pkde1d(const NumericVector x, 
-                          const NumericVector xev, 
-                          const double xmin, 
-                          const double xmax, 
-                          const double bw) 
+NumericVector eval_pkde1d(const NumericVector x,
+                          const NumericVector xev,
+                          const double xmin,
+                          const double xmax,
+                          const double bw)
 {
     NumericVector tmp(xev.size());
     NumericVector out(xev.size());
     NumericVector xeveff(xev.size());
     NumericVector xevdst(xev.size());
     double n = x.size();
-    
+
     for (int i = 0; i < xev.size(); ++i) {
-        if (xev[i] < xmin) {
+        if (xev[i] <= xmin) {
             out[i] = 0;
-        } else if (xev[i] > xmax) {
+        } else if (xev[i] >= xmax) {
             out[i] = 1;
         } else {
-            tmp = ikern_gauss(-(x - xev[i]) / bw); 
-            if(!(xmin != xmin))
-                tmp = tmp + ikern_gauss(-(2 * xmin - x - xev[i]) / bw);
-            if(!(xmax != xmax)) 
-                tmp = tmp + ikern_gauss(-(2 * xmax - x - xev[i]) / bw); 
+            // integrate kernels on regular data
+            tmp = ikern_gauss((xev[i] - x) / bw);
+            if (!(xmin != xmin)) {
+                // substract integral up to xmin
+                tmp = tmp - ikern_gauss((xmin - x) / bw);
+                // add integrals for reflected data below xmin
+                tmp = tmp + ikern_gauss((2 * xmin - x - xmin) / bw);
+                // substract integral up to xmin
+                tmp = tmp - ikern_gauss((2 * xmin - x - xev[i]) / bw);
+            }
+            if (!(xmax != xmax)) {
+                // add integrals for reflected data above xmax
+                tmp = tmp + ikern_gauss(-(2 * xmax - x - xev[i]) / bw);
+            }
             out[i] = sum(tmp) / n;
-        }    
+        }
     }
     return out;
 }
 
 // [[Rcpp::export]]
 NumericVector eval_qkde1d(const NumericVector x,
-                          const NumericVector qev, 
-                          const double xmin, 
-                          const double xmax, 
-                          const double bw) 
+                          const NumericVector qev,
+                          const double xmin,
+                          const double xmax,
+                          const double bw)
 {
     NumericVector out(qev.size()), x0, x1, ans, val;
     ans = 0.0, val = 0.0;
     double tol = ::fmax(1e-10 * (x1[0] - x0[0]), 1e-10);
-    
+
     for (int i = 0; i < qev.size(); ++i) {
         int br = 0;
         x0 = min(x) - 5 * bw;
@@ -147,7 +170,7 @@ NumericVector eval_qkde1d(const NumericVector x,
 
         out[i] = ans[0];
     }
-    
+
     return out;
 }
 
